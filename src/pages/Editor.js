10a77fb654a35773f7ee88c7fa7aafa7e494d388
Francis Bulus <notchera@gmail.com>
2019-12-17 11:48:48 +0100

    added relevant imports to editor file

---
+++ src/pages/Editor.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+import React, { Component } from "react";
+import ReactHtmlParser from "react-html-parser";
+import "./index.css";
+import "./fonts/HKGrotesk-Regular.woff";
+
+import EditorJs from "react-editor-js";
+
+import { EDITOR_JS_TOOLS } from "./constants";
