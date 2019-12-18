95afe63d9eec91bd681fee076a5d299150a15a99
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-12-18 08:59:50 +0100

    commented out unused code

--- src/redux-store/reducers/user-reducer.js
+++ src/redux-store/reducers/user-reducer.js
@@ -14,19 +14,8 @@ export const loginReducer = (state = initialLoginState, action) => {
     }
 };
 
-// export const setLoginSuccess = (user) => {
-//     return {
-//         type: types.LOGIN_SUCCESS,
-//         user: user
-//     };
-// }
 
-// export const setLoginFailure = (error) => {
-//     return {
-//       type: types.LOGIN_FAILURE,
-//       error: error
-//     };
-//   }
+
 
 const initialRegisterState = {
     username: "",
