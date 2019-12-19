6b0ecc606e773d3792e76b75dd60d7707bc2966b
Sarah <sarahennis805@hotmail.com>
2019-12-19 19:57:36 +0000

    Refactor Pitch section

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,14 +6,28 @@ import demo from "../assets/images/demo.svg";
 import logo from "../assets/images/logo.png";
 import ps from "../assets/images/ps.png";
 import appstr from "../assets/images/appstr.png";
-import { Link } from "react-router-dom";
+import Button from "../components/Button";
 import DefaultNav from "../components/Navigation/Default";
+import { Link } from "react-router-dom";
 
 const LandingPage = props => {
   const Main = styled.div`
     height: 100%;
     margin: auto;
     font-family: "Lato", sans-serif;
+
+    .rgstr-link {
+      width: 30%;
+      height: 6vh;
+      button {
+        width: 100%;
+        height: 100%;
+        font-size: 20px;
+        &.try-btn {
+            width: 50%;
+        }
+      }
+    }
   `;
 
   const MidContent = styled.div`
@@ -29,7 +43,7 @@ const LandingPage = props => {
     color: #205284;
     padding-left: 5%;
     h3 {
-      font-size: 36px;
+      font-size: 40px;
     }
 
     p {
@@ -43,41 +57,26 @@ const LandingPage = props => {
     }
 
     .email {
-      padding: 15px;
+      height: 6vh;
       background-color: #c6d0eb;
       width: 60%;
       border: none;
       border-radius: 5px;
       font-size: 20px;
       margin-right: 1rem;
-    }
-
-    .get-started-2 {
-      background-color: #6f85fd;
-      padding: 15px;
-      color: #ffffff;
-      border-radius: 5px;
-      font-size: 20px;
-      width: 30%;
-      border: none;
-      -webkit-transition-duration: 0.4s;
-      transition-duration: 0.4s;
-    }
-
-    .get-started-2:hover {
-      background-color: #212c4f;
-      color: #ffffff;
+      padding-left: 15px;
     }
   `;
 
   const LpImage = styled.div`
-    width: 55%;
+    max-width: 55%;
     display: flex;
     justify-content: flex-end;
     img {
       min-width: 95%;
       max-height: 60vh;
     }
+    padding-right: 5%;
   `;
 
   const Logo = styled.div`
@@ -88,43 +87,6 @@ const LandingPage = props => {
       font-size: 2rem;
       margin-left: 30px;
     }
-  `;
-  const LogIn = styled.div`
-    margin-right: 30px;
-    font-family: "HK Grotesk Light";
-    display: flex;
-
-    .button {
-      text-decoration: none;
-    }
-
-    .button.get-started {
-      background-color: #3672fb;
-      padding: 10px;
-      color: #ffffff;
-      border-radius: 5px;
-      border: none;
-      box-shadow: 0px 10px 20px;
-      -webkit-transition-duration: 0.4s;
-      transition-duration: 0.4s;
-    }
-
-    .button.get-started:hover {
-      background-color: #212c4f;
-      color: #ffffff;
-    }
-
-    .button.login {
-      color: #205284;
-      margin: 15px 30px 0 0;
-      text-decoration: none;
-    }
-  `;
-
-  const TopLp = styled.div`
-    display: flex;
-    justify-content: space-between;
-    padding-top: 8px;
   `;
 
   const Features = styled.div`
@@ -167,41 +129,25 @@ const LandingPage = props => {
   `;
 
   const Space = styled.div`
-    background-color: #c6d0eb;
-    height: 150px;
+    background-color: #f1f6fd;
+    height: 226px;
   `;
 
   const Pitch = styled.div`
-    color: #205284;
     display: flex;
     flex-direction: column;
-
+    align-items: center;
+    padding: 10vh 4vh;
+    text-align: center;
     h3 {
-      font-family: "HK Grotesk Bold";
-      font-size: 3rem;
-      margin: 50px auto;
+      font-size: 48px;
+      color: #205284;
+      margin-bottom: 2rem;
     }
-
     p {
-      font-family: "HK Grotesk Light";
-      margin: auto;
-    }
-
-    .try {
-      background-color: #3672fb;
-      padding: 10px;
-      color: #ffffff;
-      border-radius: 5px;
-      width: 180px;
-      margin: 50px auto;
-      border: none;
-      -webkit-transition-duration: 0.4s;
-      transition-duration: 0.4s;
-    }
-
-    .try:hover {
-      background-color: #212c4f;
-      color: #ffffff;
+      font-size: 24px;
+      color: #646f79;
+      margin-bottom: 2rem;
     }
   `;
 
@@ -260,7 +206,9 @@ const LandingPage = props => {
                 className="email"
                 placeholder="Enter your email address"
               />
-              <button className="get-started-2">Try for free</button>
+              <Link to="/register" className="rgstr-link">
+                <Button label="Try for free" />
+              </Link>
             </div>
           </form>
         </About>
@@ -307,18 +255,18 @@ const LandingPage = props => {
       </Features>
 
       <Demo>
-        <p>Watch Insight in action</p>
-        <img src={demo} alt="app demo" width="600px" />
+        <p>Watch Insight in Action</p>
+        <img src={demo} alt="app demo" />
       </Demo>
 
-      <Space>
-        <p></p>
-      </Space>
+      <Space></Space>
 
       <Pitch>
         <h3>Get Better Work Done</h3>
         <p>See why millions of people across 100 countries use Insight</p>
-        <button className="try">Try for free</button>
+        <Link to="/register" className="rgstr-link">
+          <Button label="Try for free" className="try-btn" />
+        </Link>
       </Pitch>
 
       <BottomMenu>
