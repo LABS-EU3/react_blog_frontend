327842174129c11b16f86b11b66db7e00e689bb2
ogwurujohnson <ogwurujohnson@gmail.com>
2019-12-19 10:23:59 +0100

    feat: add an onclick handler for default navigation to open modal

--- src/components/Navigation/Default.jsx
+++ src/components/Navigation/Default.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from '../Button';
+
 import {
   NavWrapper,
   Logo,
@@ -7,14 +8,15 @@ import {
 } from './navigation.styles';
 import insight from '../../assets/images/Insight.svg'
 
-export default function Default() {
+export default function Default(props) {
+
   return (
     <NavWrapper>
       <Logo>
         <img alt="logo" src={insight} />
       </Logo>
       <AuthButtons>
-        <ul><li>Log In</li></ul>
+        <ul><li onClick={() => props.handleOpen(true)}>Log In</li></ul>
         <Button label="Try for free" />
       </AuthButtons>
     </NavWrapper>
