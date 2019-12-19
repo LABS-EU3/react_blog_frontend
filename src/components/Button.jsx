6c0f790d0ed4a4112bba4ee484ac9ade1d47922b
ogwurujohnson <ogwurujohnson@gmail.com>
2019-12-19 09:48:16 +0100

    Feat: Create button reusable component

---
+++ src/components/Button.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Button(props) {
+  return (
+    <>
+      <ButtonD>{props.label}</ButtonD>
+    </>
+  );
+}
+
+const ButtonD = styled.button`
+  width: 90px;
+  background: #6f85fd;
+  border-radius: 3px;
+  padding: 0.7rem;
+  color: #ffffff;
+  font-weight: 600;
+  font-size: 14px;
+  border: none;
+  cursor: pointer;
+
+  &:focus {
+    background: #5567cb;
+  }
+  &:hover {
+    background: #5567cb;
+  }
+`;
