3b697383b159e29e47b50fa0a741fafea2510a73
ogwurujohnson <ogwurujohnson@gmail.com>
2019-12-19 21:59:58 +0100

    feat: style different variations of the footer

---
+++ src/components/Footer/footer.style.js
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+export const PrimaryFooterWrapper = styled.footer`
+  min-height: 302px;
+  background: #1d243b;
+
+  padding: 4rem;
+  padding-left: 1rem;
+
+  footer {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    div {
+      display: flex;
+      flex-direction: column;
+      h3 {
+        font-size: 15px;
+        font-weight: 500;
+        display: flex;
+        align-items: center;
+        text-align: center;
+        color: #646f79;
+      }
+
+      p {
+        font-size: 14px;
+        line-height: 24px;
+        /* identical to box height */
+        display: flex;
+        align-items: center;
+        color: #ffffff;
+        margin-top: 2rem;
+        font-weight: 500;
+      }
+
+      img {
+        width: 30px;
+        height: 50px;
+      }
+    }
+  }
+`;
+
+export const SecondaryFooterWrapper = styled.footer`
+  background: #323c5c;
+  min-height: 130px;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  div {
+    display: flex;
+    width: 290px;
+    justify-content: space-between;
+  }
+
+  p {
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 31px;
+    display: flex;
+    align-items: center;
+    color: #eef2f4;
+  }
+`;
