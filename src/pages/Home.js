bdcb554881c9ef4017ea3f60232f01fff982f729
ogwurujohnson <ogwurujohnson@gmail.com>
2019-12-19 10:23:17 +0100

    fix: move modal to only where it is needed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Default from '../components/Navigation/Default';
 import Authed from '../components/Navigation/Authed';
+import Modal from '../components/Modal';
+import Login from './Login';
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
-      <Default />
+      <Default handleOpen={setOpen}/>
       <Authed />
+
+      {open && (
+        <Modal handleOpen={setOpen}><Login /></Modal>
+      )}
     </div>
   )
 }
