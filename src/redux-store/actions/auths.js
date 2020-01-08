29cc85abd0e2c6bebdf0f2a4e747ad4a1243590e
Johnson Ogwuru <ogwurujohnson@gmail.com>
2020-01-08 22:23:24 +0100

    remove redundancies

--- src/redux-store/actions/auths.js
+++ src/redux-store/actions/auths.js
@@ -4,20 +4,20 @@ import axios from "axios";
 export const register = user => dispatch => {
   dispatch({ type: types.REGISTER_REQUEST });
   axios
-    .post("http://localhost:3300/api/auth/register", user)
+    .post("http://localhost:5000/api/auth/register", user)
     .then(res => {
       dispatch({ type: types.REGISTER_SUCCESS, payload: res.data.token, user: res.data });
     })
     .catch(err => {
       console.log(err);
-      dispatch({ type: types.REGISTER_SUCCESS });
+      dispatch({ type: types.REGISTER_FAILURE });
     });
 };
 
 export const login = user => dispatch => {
   dispatch({ type: types.LOGIN_START })
   return axios
-    .post("http://localhost:3300/api/auth/login", user)
+    .post("http://localhost:5000/api/auth/login", user)
     .then(res => {
       dispatch({ type: types.LOGIN_SUCCESS, payload: res.data.token, user: res.data });
     })
