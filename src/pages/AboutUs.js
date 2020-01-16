a0484553cac36241fcebf40967c3ad5be8cc352a
damilola.olawumi <damilola_olawumi@yahoo.com>
2020-01-16 17:18:48 +0100

    created a styles component and imported to about us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -5,58 +5,8 @@ import megan from "../assets/images/megan.png";
 import francis from "../assets/images/francis.png";
 import uzoamaka from "../assets/images/uzoamaka.jpg";
 import johnson from "../assets/images/johnson.jpg";
-import styled from "styled-components";
+import { MainDiv, Section, Img, Div, P, H6, H4, Anchor } from "../utilities/styles/AboutUs-styles";
 
-const MainDiv = styled.div`
-/* background-color:#04203D; */
-/* background-image: linear-gradient(#cc2b5e, #753a88 );  */
-width:100%;
-`;
-
-const Section = styled.section`
-display:flex;
-width: 100%;
-flex-wrap: wrap;
-text-align: center;
-
-`;
-
-const Img = styled.img`
-width:280px;
-border-radius: 50%;
-`;
-
-const Div = styled.div`
-background: #fffefe;
-width: 400px;
-margin: auto;
-height: 425px;
-padding-top: 10px;
-margin-top: 20px;
-`;
-
-const P = styled.p`
-font-size: 1.36rem;
-text-align: center;
-margin:5px;
-`;
-
-const H6 = styled.h6`
-font-size: 2.3em;
-font-family: cursive;
-margin-top: 10px;
-`;
-
-const H4 = styled.h4`
-text-align: center;
-font-size: 2.8rem;
-margin-top: 15px;
-`;
-
-const Anchor = styled.a`
-margin: 15px;
-font-size: 13px;
-`;
 
 function AboutUs() {
 
---
+++ src/utilities/styles/AboutUs-styles.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+/* background-color:#04203D; */
+/* background-image: linear-gradient(#cc2b5e, #753a88 );  */
+width:100%;
+`;
+
+export const Section = styled.section`
+display:flex;
+width: 100%;
+flex-wrap: wrap;
+text-align: center;
+
+`;
+
+export const Img = styled.img`
+width:280px;
+border-radius: 50%;
+`;
+
+export const Div = styled.div`
+background: #fffefe;
+width: 400px;
+margin: auto;
+height: 425px;
+padding-top: 10px;
+margin-top: 20px;
+`;
+
+export const P = styled.p`
+font-size: 1.36rem;
+text-align: center;
+margin:5px;
+`;
+
+export const H6 = styled.h6`
+font-size: 2.3em;
+font-family: cursive;
+margin-top: 10px;
+`;
+
+export const H4 = styled.h4`
+text-align: center;
+font-size: 2.8rem;
+margin-top: 15px;
+`;
+
+export const Anchor = styled.a`
+margin: 15px;
+font-size: 13px;
+`;
