f0018f5d3d2c266f08196c91df354c5456ff9973
damilola.olawumi <damilola_olawumi@yahoo.com>
2020-01-16 15:15:07 +0100

    wip

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -9,6 +9,7 @@ import styled from "styled-components";
 
 const MainDiv = styled.div`
 /* background-color:#04203D; */
+/* background-image: linear-gradient(#F5F7FA, #FFFFFF );  */
 width:100%;
 `;
 
@@ -18,7 +19,7 @@ width: 100%;
 flex-wrap: wrap;
 text-align: center;
  /* background-image: linear-gradient(#8693AB, #BDD4E7 );  */
-/* background-image: linear-gradient(#F5F7FA, #FFFFFF ); */
+
 `;
 
 const Img = styled.img`
@@ -30,7 +31,7 @@ const Div = styled.div`
 background: #fffefe;
 width: 400px;
 margin: auto;
-box-shadow: 0px 1px 3px 0px #aaa;
+/* box-shadow: 0px 1px 3px 0px #aaa; */
 height: 425px;
 padding-top: 10px;
 margin-top: 20px;
@@ -38,9 +39,10 @@ margin-top: 20px;
 
 const P = styled.p`
 font-size: 1.36rem;
-text-align: left;
-margin-left: 15px;
-margin-top: 15px;
+text-align: center;
+/* margin-left: 15px;
+margin-top: 15px; */
+margin:5px;
 `;
 
 const H6 = styled.h6`
@@ -56,6 +58,11 @@ margin-top: 15px;
 /* color:white; */
 `;
 
+const Anchor = styled.a`
+margin: 15px;
+font-size: 13px;
+`;
+
 function AboutUs() {
 
     return (
@@ -66,11 +73,11 @@ function AboutUs() {
                 <Div>
                     <Img src={dammy} alt="dammy.png" />
                     <H6>Damilola Olawumi</H6>
-                    <P>From Nigeria, Damilola is a full stack web developer. Skilled in Javascript, React and Nodejs. I am committed to a lifelong learning, creating value and making positive impact to my community.</P>
+                    <P>From Nigeria, Damilola is a full stack web developer. I am committed to a lifelong learning, creating value and making positive impact to my community.</P>
                     <ul>
                         <li>
-                            <a href="https://github.com/Damilolawumi" target="_blank" rel="noopener noreferrer">Github </a> <br />
-                            <a href="https://www.linkedin.com/in/damilola-olawumi/" target="_blank" rel="noopener noreferrer">LinkedIn </a>
+                            <Anchor href="https://github.com/Damilolawumi" target="_blank" rel="noopener noreferrer">Github </Anchor> 
+                            <Anchor href="https://www.linkedin.com/in/damilola-olawumi/" target="_blank" rel="noopener noreferrer">LinkedIn </Anchor>
                         </li>
                     </ul>
                 </Div>
@@ -81,8 +88,8 @@ function AboutUs() {
                     <P>Ghanaian, full stack Web developer, avid fan of sports and video games. Striving to improve each day.</P>
                     <ul>
                         <li>
-                            <a href="https://github.com/davidkuseh" target="_blank" rel="noopener noreferrer">Github </a> <br />
-                            <a href="https://www.linkedin.com/in/david-kuseh" target="_blank" rel="noopener noreferrer">LinkedIn </a>
+                            <Anchor href="https://github.com/davidkuseh" target="_blank" rel="noopener noreferrer">Github </Anchor> 
+                            <Anchor href="https://www.linkedin.com/in/david-kuseh" target="_blank" rel="noopener noreferrer">LinkedIn </Anchor>
                         </li>
                     </ul>
                 </Div>
@@ -93,8 +100,8 @@ function AboutUs() {
                     <P>Full Stack Web Development student from Dublin with an interest in UI effects, I enjoy bringing pages to life to create unique and dynamic user experiences.</P>
                     <ul>
                         <li>
-                            <a href="https://github.com/Megan0145" target="_blank" rel="noopener noreferrer">Github </a> <br />
-                            <a href="https://www.linkedin.com/in/megan-ennis-1391a8124/" target="_blank" rel="noopener noreferrer">LinkedIn </a>
+                            <Anchor href="https://github.com/Megan0145" target="_blank" rel="noopener noreferrer">Github </Anchor> 
+                            <Anchor href="https://www.linkedin.com/in/megan-ennis-1391a8124/" target="_blank" rel="noopener noreferrer">LinkedIn </Anchor>
                         </li>
                     </ul>
                 </Div>
@@ -102,11 +109,11 @@ function AboutUs() {
                 <Div>
                     <Img src={francis} alt="francis.png" />
                     <H6>Francis Bulus</H6>
-                    <P>From Nigeria, Francis is a full stack web developer. skilled in Javascript, React and Nodejs. I am committed to a lifelong learning, creating value and making positive impact to my community.</P>
+                    <P>From Nigeria, Francis is a full stack web developer. I am committed to a lifelong learning, creating value and making positive impact to my community.</P>
                     <ul>
                         <li>
-                            <a href="https://github.com/fosterferret" target="_blank" rel="noopener noreferrer">Github </a> <br />
-                            <a href="https://www.linkedin.com/in/francis-bulus-b61098191/" target="_blank" rel="noopener noreferrer">LinkedIn </a>
+                            <Anchor href="https://github.com/fosterferret" target="_blank" rel="noopener noreferrer">Github </Anchor> 
+                            <Anchor href="https://www.linkedin.com/in/francis-bulus-b61098191/" target="_blank" rel="noopener noreferrer">LinkedIn </Anchor>
                         </li>
                     </ul>
                 </Div>
@@ -117,8 +124,8 @@ function AboutUs() {
                     <P>Uzoamaka Anyanwu is a fullstack developer, who is skilled in JavaScript, React, Vue, NodeJs, Postgres. I love building products and is dedicated to finding ways to create better user experiences.</P>
                     <ul>
                         <li>
-                            <a href="https://github.com/Uzoamaka126" target="_blank" rel="noopener noreferrer">Github </a> <br />
-                            <a href="https://www.linkedin.com/in/grace-anyanwu-ga/" target="_blank" rel="noopener noreferrer">LinkedIn </a>
+                            <Anchor href="https://github.com/Uzoamaka126" target="_blank" rel="noopener noreferrer">Github </Anchor> 
+                            <Anchor href="https://www.linkedin.com/in/grace-anyanwu-ga/" target="_blank" rel="noopener noreferrer">LinkedIn </Anchor>
                         </li>
                     </ul>
                 </Div>
@@ -126,11 +133,11 @@ function AboutUs() {
                 <Div>
                     <Img src={johnson} alt="johnson.jpg" />
                     <H6>Johnson Ogwuru</H6>
-                    <P>Johnson is a software Engineer and a passionate problem solver. He seeks out ways to be exposed to evolving global best practices, and work with smart people who understand his desire to use technology to affect change for millions of people around the world.</P>
+                    <P>Johnson is a software Engineer and a passionate problem solver. He loves to work with people.</P>
                     <ul>
                         <li>
-                            <a href="https://github.com/ogwurujohnson" target="_blank" rel="noopener noreferrer">Github </a> <br />
-                            <a href="https://linkedin.com/in/ogwurujohnson" target="_blank" rel="noopener noreferrer">LinkedIn </a>
+                            <Anchor href="https://github.com/ogwurujohnson" target="_blank" rel="noopener noreferrer">Github </Anchor> 
+                            <Anchor href="https://linkedin.com/in/ogwurujohnson" target="_blank" rel="noopener noreferrer">LinkedIn </Anchor>
                         </li>
                     </ul>
                 </Div>
