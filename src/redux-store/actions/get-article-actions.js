f9dc6668f0fe124fcd171fb7b0fe355cad5305b3
Johnson Ogwuru <ogwurujohnson@gmail.com>
2020-01-22 18:47:26 +0100

    implement an action creator for fetching single article

--- src/redux-store/actions/get-article-actions.js
+++ src/redux-store/actions/get-article-actions.js
@@ -1,7 +1,10 @@
 import {
   GET_ALL_ARTICLES_START,
   GET_ALL_ARTICLES_SUCCESS,
-  GET_ALL_ARTICLES_FAIL
+  GET_ALL_ARTICLES_FAIL, 
+  GET_SINGLE_START,
+  GET_SINGLE_SUCCESS,
+  GET_SINGLE_FAIL
 } from "./types";
 import { apiURL } from '../../utilities/urls';
 
@@ -19,3 +22,17 @@ export const getArticleFeed = () => async dispatch => {
     dispatch({ type: GET_ALL_ARTICLES_FAIL });
   }
 };
+
+
+export const getSingleArticle = (id) => async dispatch => {
+  dispatch({ type: GET_SINGLE_START });
+  try {
+    const response = await axiosWithAuth()
+      .get(`${apiURL}/articles/${id}`);
+    dispatch({ type: GET_SINGLE_SUCCESS, payload: response.data.response });
+  }
+  catch (err) {
+    console.log(err);
+    dispatch({ type: GET_SINGLE_FAIL });
+  }
+}
