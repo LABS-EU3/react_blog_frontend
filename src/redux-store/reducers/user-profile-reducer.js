23979d40b84e218d479f6fca9baf2a184d31e6bb
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-23 17:36:24 +0000

    Refactor reducer to include new action types. Add extra state slice for user interests

--- src/redux-store/reducers/user-profile-reducer.js
+++ src/redux-store/reducers/user-profile-reducer.js
@@ -7,18 +7,26 @@ import {
   UPDATE_USER_PROFILE_FAIL,
   GET_TAGS_START,
   GET_TAGS_SUCCESS,
-  GET_TAGS_FAIL
+  GET_TAGS_FAIL,
+  UPDATE_USER_INTERESTS_START,
+  REMOVE_USER_INTERESTS_SUCCESS,
+  ADD_USER_INTERESTS_SUCCESS,
+  UPDATE_USER_INTERESTS_FAIL
 } from "../actions/types";
 
 export const initState = {
   loading: false,
   data: [],
-  tags: []
+  tags: [],
+  interests: []
 };
 
 export const userProfileReducer = (state = initState, action) => {
   switch (action.type) {
-    case GET_USER_PROFILE_START || UPDATE_USER_PROFILE_START || GET_TAGS_START:
+    case GET_USER_PROFILE_START ||
+      UPDATE_USER_PROFILE_START ||
+      GET_TAGS_START ||
+      UPDATE_USER_INTERESTS_START:
       return {
         ...state,
         loading: true
@@ -27,15 +35,44 @@ export const userProfileReducer = (state = initState, action) => {
       return {
         ...state,
         loading: false,
-        data: action.payload
+        data: action.payload,
+        interests: action.payload.interests
       };
-      case GET_TAGS_SUCCESS:
-        return {
-          ...state,
-          loading: false,
-          tags: action.payload
-        };
-    case GET_USER_PROFILE_FAIL || UPDATE_USER_PROFILE_FAIL || GET_TAGS_FAIL:
+    case GET_TAGS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        tags: action.payload
+      };
+    case ADD_USER_INTERESTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        interests: [
+          ...state.interests,
+          ...action.payload.map(interest => {
+            return { name: interest };
+          })
+        ]
+      };
+    case REMOVE_USER_INTERESTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        interests: [
+          state.interests.map(interest => {
+            action.payload.map(i => {
+              if (i !== interest.name) {
+                return { name: interest.name };
+              }
+            });
+          })
+        ]
+      };
+    case GET_USER_PROFILE_FAIL ||
+      UPDATE_USER_PROFILE_FAIL ||
+      GET_TAGS_FAIL ||
+      UPDATE_USER_INTERESTS_FAIL:
       return {
         ...state,
         loading: false
