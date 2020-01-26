cbe814a9feba9611af1fac5c022f97ea3c965f4c
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-26 17:29:29 +0000

    Add test:Displays input field for user to edit their username when in edit mode

--- src/__tests__/editprofile.test.js
+++ src/__tests__/editprofile.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 import EditProfile from "../pages/EditProfile.js";
 import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
@@ -46,5 +46,20 @@ describe("Edit Profile component", () => {
     });
     const btn = getByText(/Edit Profile/i);
     expect(btn).toBeInTheDocument();
+  });
+  it("Renders without crashing", () => {
+    const dummyToken =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoxLCJpYXQiOjE1ODAwNTU3MjgsImV4cCI6MTU4MDE0MjEyOH0.KJBwGnrLKdhQRX4RZP7_bPnFLbYNGmTdpjmkh5YH1a8";
+    localStorage.setItem("token", dummyToken);
+    const { getByText, getByPlaceholderText } = renderWithRedux(
+      <EditProfile />,
+      {
+        initialState: { userProfile: defaultState }
+      }
+    );
+    const btn = getByText(/Edit Profile/i);
+    fireEvent.click(btn, "click");
+    const fullnameInputField = getByPlaceholderText("Full Name");
+    expect(fullnameInputField).toBeInTheDocument();
   });
 });
