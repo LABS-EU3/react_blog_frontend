a935c692de08c79ce1e05d2cdfb97b1385d7c0e8
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-28 14:16:13 +0000

    Add buttons to each article card to invoke action when clicked. Refactor function to redirect to single article view

--- src/pages/UserArticles/UserArticleLIst.js
+++ src/pages/UserArticles/UserArticleLIst.js
@@ -1,80 +1,89 @@
 /* eslint-disable no-shadow */
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { Link, useHistory } from 'react-router-dom';
-import { getAuthorArticle } from '../../redux-store/actions/get-author-article';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { Link, useHistory } from "react-router-dom";
+import {
+  getAuthorArticle,
+  deleteAuthorArticle
+} from "../../redux-store/actions/get-author-article";
 import blue_arrow from "../../assets/images/Icons/blue_arrow.svg";
-import {MainDiv} from "../../utilities/styles/profile-styles";
+import { MainDiv } from "../../utilities/styles/profile-styles";
 
 export const UserArticleList = ({
-    isFetching,
-    getAuthorArticle,
-    // history,
-    userArticles: { userArticles },
-    
+  isFetching,
+  getAuthorArticle,
+  deleteAuthorArticle,
+  userArticles: { userArticles }
 }) => {
-    useEffect(() => {
-              getAuthorArticle();
+  useEffect(() => {
+    getAuthorArticle();
+  }, []);
 
-    }, []);
+  const history = useHistory();
 
-    const history = useHistory();
+  const handleDelete = e => {
+    deleteAuthorArticle(e.target.id);
+  };
 
-    return (
-        <MainDiv>
-            {!isFetching ? (
-                <section className="new-section">
-                    {userArticles.length !== 0 ? (
-                        userArticles.response.map(article => (
-                            // <UserArticleCard
-
-                            <div
-                          className="main-article"
-                          key={article.id}
-                          onClick={() =>
-                            history.push(`/article/${article.custom_id}`)
-                          }
-                        >
-                          <img src={article.coverImageUrl} alt="" />
-                          <div className="main-article-content">
-                            <h3>{article.title}</h3>
-                            <p>
-                              {JSON.parse(article.body)
-                                .find(block => block.type === "paragraph")
-                                .data.text.substring(0, 150)}
-                              ...
-                            </p>
-                            <div className="main-article-footer">
-                              <p>{article.author}</p>
-                              <div className="article-link">
-                                <Link to={`/article/${article.custom_id}`}>Details</Link>
-                                <img src={blue_arrow} alt="Blue Arrow" />
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                            
-                                // key={`${articles.id}`}
-                                // title={articles.title}
-                                // body={articles.body}
-                                // id={articles.id}
-                                // photo={articles.coverImageUrl}
-                            
-                        ))
-                    ) : (
-                            <div className="empty-state">
-                                <p>No data to display</p>
-                            </div>
-                        )}
-                </section>
-            ) : (
-                    <div className="empty-state">
-                       
+  return (
+    <MainDiv>
+      {!isFetching ? (
+        <section className="new-section">
+          {userArticles.length !== 0 ? (
+            userArticles.response.map(article => (
+              <div
+                className="main-article"
+                key={article.id}
+                onClick={e =>
+                  e.target.name !== "delete" &&
+                  history.push(`/article/${article.custom_id}`)
+                }
+              >
+                <img src={article.coverImageUrl} alt="" />
+                <div className="main-article-content">
+                  <h3>{article.title}</h3>
+                  <p>
+                    {JSON.parse(article.body)
+                      .find(block => block.type === "paragraph")
+                      .data.text.substring(0, 150)}
+                    ...
+                  </p>
+                  <div className="main-article-footer">
+                      <button
+                        onClick={handleDelete}
+                        id={article.id}
+                        name="delete"
+                        className="delete"
+                      >
+                        Delete
+                      </button>
+                    <div className="article-link">
+                      <Link to={`/article/${article.custom_id}`}>Details</Link>
+                      <img src={blue_arrow} alt="Blue Arrow" />
                     </div>
-                )}
-        </MainDiv>
-    );
+                  </div>
+                </div>
+              </div>
+
+              // key={`${articles.id}`}
+              // title={articles.title}
+              // body={articles.body}
+              // id={articles.id}
+              // photo={articles.coverImageUrl}
+            ))
+          ) : (
+            <div className="empty-state">
+              <p>No data to display</p>
+            </div>
+          )}
+        </section>
+      ) : (
+        <div className="empty-state"></div>
+      )}
+    </MainDiv>
+  );
 };
-export default connect(state => state, { getAuthorArticle })(
-    UserArticleList
-);
+export default connect(state => state, {
+  getAuthorArticle,
+  deleteAuthorArticle
+})(UserArticleList);
