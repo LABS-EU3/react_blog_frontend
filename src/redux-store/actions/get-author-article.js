560db0e35e9e5f2f39197191351800ec6b6551b9
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-28 16:07:00 +0000

    Pass in dynamic user id to getAuthorArticle action fn

--- src/redux-store/actions/get-author-article.js
+++ src/redux-store/actions/get-author-article.js
@@ -5,7 +5,7 @@ export const getAuthorArticle = id => async dispatch => {
   dispatch({ type: types.GET_USER_ARTICLES_START });
   try {
     const response = await axios.get(
-      `http://localhost:5000/api/articles/author/1`
+      `http://localhost:5000/api/articles/author/${id}`
     );
     dispatch({ type: types.GET_USER_ARTICLES_SUCCESS, payload: response.data });
   } catch (err) {
