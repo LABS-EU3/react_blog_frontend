75e95c306db272c8145ded9a5a78e66804fe7f51
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-28 14:13:30 +0000

    Add action to delete article

--- src/redux-store/actions/get-author-article.js
+++ src/redux-store/actions/get-author-article.js
@@ -2,12 +2,30 @@ import * as types from "./types";
 import axios from "axios";
 
 export const getAuthorArticle = id => async dispatch => {
-    dispatch({ type: types.GET_USER_ARTICLES_START });
-    try {
-      const response = await axios.get(`http://localhost:5000/api/articles/author/1`);
-      dispatch({ type: types.GET_USER_ARTICLES_SUCCESS, payload: response.data });
-    } catch (err) {
-      console.log(err);
-      dispatch({ type: types.GET_USER_ARTICLES_FAIL });
-    }
-  };
+  dispatch({ type: types.GET_USER_ARTICLES_START });
+  try {
+    const response = await axios.get(
+      `http://localhost:5000/api/articles/author/1`
+    );
+    dispatch({ type: types.GET_USER_ARTICLES_SUCCESS, payload: response.data });
+  } catch (err) {
+    console.log(err);
+    dispatch({ type: types.GET_USER_ARTICLES_FAIL });
+  }
+};
+
+export const deleteAuthorArticle = id => async dispatch => {
+  dispatch({ type: types.DELETE_USER_ARTICLES_START });
+  try {
+    const response = await axios.delete(
+      `http://localhost:5000/api/articles/${id}`
+    );
+    dispatch({
+      type: types.DELETE_USER_ARTICLES_SUCCESS,
+      payload: response.data.articleId
+    });
+  } catch (err) {
+    console.log(err);
+    dispatch({ type: types.DELETE_USER_ARTICLES_FAIL });
+  }
+};
