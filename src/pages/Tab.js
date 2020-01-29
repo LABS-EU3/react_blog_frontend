835bceea82d324f16546def9e3d9e9a6841cc86c
Francis Bulus <notchera@gmail.com>
2020-01-29 15:23:09 +0100

    building out tabs container

--- src/pages/Tab.js
+++ src/pages/Tab.js
@@ -1,32 +1,4 @@
-import React, { Component } from "react";
-
-// class Tab extends Component {
-//   onClick = () => {
-//     const { label, onClick } = this.props;
-//     onClick(label);
-//   };
-
-//   render() {
-//     const {
-//       onClick,
-//       props: { activeTab, label }
-//     } = this;
-
-//     let className = "tab-list-item";
-
-//     if (activeTab === label) {
-//       className += " tab-list-active";
-//     }
-
-//     return (
-//       <li className={className} onClick={onClick}>
-//         {label}
-//       </li>
-//     );
-//   }
-// }
-
-// export default Tab;
+import React from "react";
 
 const Tab = props => {
   const { label, onClick, activeTab } = props;
--- src/pages/Tabs.js
+++ src/pages/Tabs.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 import Tab from "./Tabs";
 
 class Tabs extends Component {
