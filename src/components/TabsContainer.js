9977cc032759440bdf8859a21d3c8849d31eecab
Francis Bulus <notchera@gmail.com>
2020-01-29 15:25:57 +0100

    set up tabs container - will revisit later to style

---
+++ src/components/TabsContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Tabs from "./Tabs";
+
+const Container = () => {
+  return (
+    <Tabs>
+      <div label="Insight">Insights</div>
+      <div label="People">People</div>
+    </Tabs>
+  );
+};
+
+export default Container;
