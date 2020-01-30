bc11308ec33c180edf486fd166611557a7c4a6d2
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-30 18:40:40 +0000

    Add action to follow users

--- src/redux-store/actions/onboarding-actions.js
+++ src/redux-store/actions/onboarding-actions.js
@@ -4,7 +4,10 @@ import {
   GET_ALL_TAGS_FAIL,
   GET_USERS_TO_FOLLOW_START,
   GET_USERS_TO_FOLLOW_SUCCESS,
-  GET_USERS_TO_FOLLOW_FAIL
+  GET_USERS_TO_FOLLOW_FAIL,
+  FOLLOW_USERS_START,
+  FOLLOW_USERS_SUCCESS,
+  FOLLOW_USERS_FAIL
 } from "./types";
 
 import axios from "axios";
@@ -34,3 +37,19 @@ export const getUsersToFollow = () => async dispatch => {
     dispatch({ type: GET_USERS_TO_FOLLOW_FAIL });
   }
 };
+
+export const followUsers = (followIds) => async dispatch => {
+  dispatch({ type: FOLLOW_USERS_START });
+  try {
+    const response = await axiosWithAuth().post(
+      `http://localhost:5000/api/follows`, followIds
+    );
+    dispatch({ type: FOLLOW_USERS_SUCCESS});
+    console.log(response);
+
+  } catch (err) {
+    console.log(err);
+    dispatch({ type: FOLLOW_USERS_FAIL });
+  }
+
+}
