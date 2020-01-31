4080571b9c0337b84d991352dfddf66232c4b461
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-31 12:09:59 +0000

    Profile loosely styled and dynamically rendering

--- src/pages/AuthorProfile.js
+++ src/pages/AuthorProfile.js
@@ -1,23 +1,73 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { getAuthorProfile } from "../redux-store/actions/get-author-profile-actions";
+import DefaultNavigation from "../components/Navigation/Default";
+import AuthedNavigation from "../components/Navigation/Authed";
+import { getToken } from "../utilities/authentication";
+import Button from "../components/Buttons/Button";
+import { StyledProfile, StyledInfo, StyledStories } from "../utilities/styles/author-profile-styles";
 
 const AuthorProfile = props => {
   const { match, getAuthorProfile, profile } = props;
+  const token = getToken();
   useEffect(() => {
     const authorId = match.params.id;
     getAuthorProfile(authorId);
   }, [match.params.id, getAuthorProfile]);
-  return <div>
-      {profile && console.log(profile)}
-      AuthorProfile
-      </div>;
+
+  return (
+    <div>
+      {token ? <AuthedNavigation /> : <DefaultNavigation />}
+      <StyledProfile>
+        {profile && (
+          <>
+            <div className="section info">
+              <StyledInfo>
+                <div className="content">
+                  <h3>{profile.fullname}</h3>
+                  <p className="bio">
+                    {profile.bio
+                      ? profile.bio
+                      : "MeganEnnis.com Author, essayist, screenwriter, copywriter, movie-maker, beach-lover, bison-lover, gym-goer, all-around-the-world-goer."}
+                  </p>
+                  {profile.interests && (
+                    <div className="interests">
+                      {profile.interests.map(interest => (
+                        <p>#{interest.name}</p>
+                      ))}
+                    </div>
+                  )}
+                  <div className="follows">
+                    <p>
+                      <span>{profile.followers}</span> Followers
+                    </p>
+                    <p>
+                      <span>{profile.following}</span> Following
+                    </p>
+                  </div>
+                  {token && <Button label="Follow" />}                  
+                </div>
+                <div className="image">
+                  <img src={profile.avatarUrl} alt="" />
+                </div>
+              </StyledInfo>
+            </div>
+            <div className="section stories">
+              <StyledStories>
+                <h3>Stories Here</h3>
+              </StyledStories>
+            </div>
+          </>
+        )}
+      </StyledProfile>
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
-    return {
-      profile: state.authorProfile.profile
-    };
+  return {
+    profile: state.authorProfile.profile
   };
+};
 
-export default connect(mapStateToProps, {getAuthorProfile})(AuthorProfile);
+export default connect(mapStateToProps, { getAuthorProfile })(AuthorProfile);
