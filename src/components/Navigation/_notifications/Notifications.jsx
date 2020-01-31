232e10c313a123133585a72466add91fefb2b348
Uzoamaka126 <vanessapeters126@gmail.com>
2020-01-31 10:09:50 +0100

    Setup pusher for client

--- src/components/Navigation/_notifications/Notifications.jsx
+++ src/components/Navigation/_notifications/Notifications.jsx
@@ -1,49 +1,57 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell } from '@fortawesome/free-solid-svg-icons'
 import Dropdown from '../NavigationDropdown';
 import { StyledNotificationIcon, StyledNotificationDropdownContent } from '../../styles/StyledNotification';
 import TinyAvatar from '../../../assets/random-avatar.jpeg'
+import Pusher from 'pusher-js';
+import { getNotifications} from '../../../redux-store/actions/get-notifications';
+import { getToken } from '../../../utilities/authentication'
+import { pusher, notifications_channel } from '../../../utilities/pusherConfiguration';
 
 export default function ProfileImageDropdown(props) {
-    const [state, setState] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const [notifications, setNotifications] = useState([]);
 
-    console.log(props);
+    // fetch the initial notifications
+    useEffect(() => {
+        getNotifications()
+    }, [getNotifications]);
+
+
+    notifications_channel.bind('new-notification', data => {
+        setNotifications(notifications.concat(data.notification))
+    }, this);
+
+
     function toggleDropdown() {
-        setState(!state);
+        setIsOpen(!isOpen);
             document.getElementById('dropdown-notifications-content').classList.toggle('show');
     }
+   
+
+
     
     return (
         <Dropdown>
             <StyledNotificationIcon>
                 <FontAwesomeIcon icon={faBell}  onClick={toggleDropdown}/>
             </StyledNotificationIcon>
-             <StyledNotificationDropdownContent className="dropdown-content" id="dropdown-notifications-content">
+             <StyledNotificationDropdownContent  className="dropdown-content" id="dropdown-notifications-content">
                  <ul>
-                    <li className="dropdown-user-bio card">
-                        <div className="body">
-                            <div className="img-wrapper">
-                                <img alt="avatar" src={TinyAvatar}/>
+                     {notifications.map(notification => (
+                        <li className="dropdown-user-bio card">
+                            <div className="body">
+                                <div className="img-wrapper">
+                                    <img alt="avatar" src={TinyAvatar}/>
+                                </div>
+                                <div className="activity-wrapper">
+                                    <p><strong>DammyOluwa</strong> <span>started following you</span></p>
+                                    <p>Date here</p>
+                                </div>
                             </div>
-                            <div className="activity-wrapper">
-                                <p><strong>DammyOluwa</strong> <span>started following you</span></p>
-                                <p>Date here</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li className="dropdown-user-bio card">
-                        <div className="body">
-                            <div className="img-wrapper">
-                                <img alt="avatar" src={TinyAvatar}/>
-                            </div>
-                            <div className="activity-wrapper">
-                                <p><strong>DammyOluwa</strong> <span>started following you</span></p>
-                                <p>Date here</p>
-                            </div>
-                        </div>
-                    </li>
+                        </li>
+                     ))}
                  </ul>
             </StyledNotificationDropdownContent>
         </Dropdown>
