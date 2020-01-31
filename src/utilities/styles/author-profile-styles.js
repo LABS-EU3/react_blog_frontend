5fd44bd3f04c7cb357e30941db5a2096e5e88927
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-31 15:30:28 +0000

    Add styles for articles

--- src/utilities/styles/author-profile-styles.js
+++ src/utilities/styles/author-profile-styles.js
@@ -8,13 +8,18 @@ export const StyledProfile = styled.div`
   padding: 3rem 0;
   background-color: white;
   .section {
-    width: 100%;
+    width: 90%;
     display: flex;
     flex-direction: column;
     align-items: center;
     padding: 3rem 15vw;
-    &.info {
+    &.shorter {
       height: 50vh;
+    }
+  }
+  .stories {
+    h3{ 
+      width: 100%;
     }
   }
 `;
@@ -74,4 +79,80 @@ export const StyledStories = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  .article {
+    width: 100%;
+    margin: 2rem 0;
+    margin-right: 1.3rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.13);
+    &:hover {
+      cursor: pointer;
+      box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.20);
+    }
+    &.dynamic {
+      width: 30%;
+    }
+
+    img {
+      width: 100%;
+      height: 30vh;
+      border-radius: 3px 3px 0px 0px;
+      margin-bottom: -10rem;
+      z-index: 1;
+    }
+
+    .main-article-content {
+      background: #f7f9fb;
+      border-radius: 5px;
+      width: 100%;
+      padding: 2rem;
+      padding-top: 3rem;
+
+      h3 {
+        margin: 8rem 0 2rem 0;
+        font-family: Lato;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 25px;
+        line-height: 35px;
+      }
+      p {
+        font-family: Lato;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 14px;
+        line-height: 23px;
+        color: #b7bbc0;
+        margin-bottom: 5rem;
+      }
+      .main-article-footer {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        p {
+          font-style: italic;
+          margin-bottom: 0;
+        }
+        .article-link {
+          display: flex;
+          height: 2vh;
+          align-items: center;
+          img {
+            height: 20px;
+            width: 14px;
+            margin: 0 0 0 0.8rem;
+          }
+          a {
+            color: #3652BF;
+            font-size: 13px;
+            font-weight: 500;
+          }
+        }
+      }
+    }
+  }
+
 `;
