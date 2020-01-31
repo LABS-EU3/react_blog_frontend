953e1b6c7d43b8a825443a6f4242d8b1b63ad872
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-31 13:05:44 +0000

    Add cases based on new actions to reducer

--- src/redux-store/reducers/author-profile-reducer.js
+++ src/redux-store/reducers/author-profile-reducer.js
@@ -1,12 +1,16 @@
 import {
   GET_AUTHOR_PROFILE_START,
   GET_AUTHOR_PROFILE_SUCCESS,
-  GET_AUTHOR_PROFILE_FAIL
+  GET_AUTHOR_PROFILE_FAIL,
+  FOLLOW_AUTHOR_START,
+  FOLLOW_AUTHOR_SUCCESS,
+  FOLLOW_AUTHOR_FAIL
 } from "../actions/types";
 
 export const initState = {
   loading: false,
-  profile: {}
+  profile: {},
+  followAuthorSuccess: false
 };
 
 export const authorProfileReducer = (state = initState, action) => {
@@ -16,16 +20,38 @@ export const authorProfileReducer = (state = initState, action) => {
         ...state,
         loading: true
       };
+    case FOLLOW_AUTHOR_START:
+      return {
+        ...state,
+        loading: true,
+        followAuthorSuccess: false
+      };
     case GET_AUTHOR_PROFILE_SUCCESS:
       return {
         ...state,
         loading: false,
         profile: action.payload
       };
+    case FOLLOW_AUTHOR_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        followAuthorSuccess: true,
+        profile: {
+          ...state.profile,
+          followers: JSON.parse(state.profile.followers) + 1
+        }
+      };
     case GET_AUTHOR_PROFILE_FAIL:
       return {
         ...state,
         loading: false
+      };
+    case FOLLOW_AUTHOR_FAIL:
+      return {
+        ...state,
+        loading: false,
+        followAuthorSuccess: false
       };
     default:
       return state;
