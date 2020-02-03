fb6a2d2cbec226ee611ba8252fe99f1cca852d30
Megan Ennis <Megan@Megans-iMac-9.local>
2020-02-03 10:43:29 +0000

    Scaffold basic structure. Containers for each section of content

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -2,17 +2,46 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import DefaultNavigation from "../components/Navigation/Default";
 import AuthedNavigation from "../components/Navigation/Authed";
-import arrow from "../assets/images/Icons/arrow_icon.svg";
-import blue_arrow from "../assets/images/Icons/blue_arrow.svg";
-import {
-  StyledFeed,
-  StyledTrending,
-  StyledLoggedInFeed,
-  StyledMainFeed
-} from "../utilities/styles/feed-styles";
 import { getArticleFeed } from "../redux-store/actions/get-article-actions";
 import { getToken } from "../utilities/authentication";
-import { Link } from "react-router-dom";
+
+import { Section, mixins } from "../styles/shared";
+import styled from "styled-components";
+import theme from "../styles/theme";
+import ArticleCard from "../components/ArticleCardWide";
+
+const Container = styled(Section)`
+  margin-top: 8rem;
+  h1 {
+    font-family: ${theme.fonts.Oswald};
+    font-size: ${theme.fontSizes.ttl};
+    padding-bottom: 1rem;
+    border-bottom: 1px solid ${theme.colors.lightGrey};
+  }
+  h2 {
+    font-family: ${theme.fonts.Oswald};
+    font-size: ${theme.fontSizes.xxl};
+  }
+`;
+
+const StyledTrending = styled.div`
+  width: 100%;
+  ${mixins.flexColumn};
+  justify-content: space-between;
+`;
+
+const StyledFeed = styled.div`
+  width: 100%;
+  ${mixins.flexBetween};
+  margin-top: 5rem;
+  align-items: flex-start;
+  .interests {
+    width: 55%;
+  }
+  .following {
+    width: 40%;
+  }
+`;
 
 export function Feed(props) {
   const { getArticleFeed, articles } = props;
@@ -23,167 +52,41 @@ export function Feed(props) {
   }, [getArticleFeed]);
 
   return (
-    <div>
+    <>
       {token ? <AuthedNavigation /> : <DefaultNavigation />}
-      <StyledFeed>
+
+      <Container>
         <StyledTrending>
-          <div className="trending-header">
-            <button>TRENDING NOW</button>
-          </div>
+          <h1>TRENDING NOW</h1>
           {!articles.loading && articles.data.trending && (
             <div className="trending-content">
-              <div
-                className="trending-content-jumbo"
-                onClick={() =>
-                  props.history.push(`/article/${articles.data.trending[0].custom_id}`)
-                }
-                style={{
-                  backgroundImage: `url(${articles.data.trending[0]
-                    .coverImageUrl})`,
-                  cursor: `pointer`
-                }}
-              >
-                <Link to={`/article/${articles.data.trending[0].custom_id}`}>
-                  {articles.data.trending[0].title}
-                </Link>
-              </div>
-
-              <div className="trending-content-regular">
-                {articles.data.trending.slice(1).map(trend => {
-                  return (
-                      <div
-                        key={trend.id}
-                        onClick={() =>
-                          props.history.push(`/article/${trend.custom_id}`)
-                        }
-                        className="content-box"
-                        style={{
-                          backgroundImage: `url(${trend
-                            .coverImageUrl})`,
-                          cursor: `pointer`
-                        }}
-                      >
-                        <Link to={`/article/${trend.custom_id}`}>
-                          {trend.title}
-                        </Link>
-                      </div>
-                  );
-                })}
-              </div>
+              <ArticleCard insight={articles.data.trending[0]} />
+              {articles.data.trending.slice(1, 3).map(article => (
+                <ArticleCard insight={article} />
+              ))}
             </div>
           )}
         </StyledTrending>
-        <StyledMainFeed>
-          {!articles.loading && (
-            <div
-              className={
-                articles.data.following || articles.data.reactions
-                  ? "main-insights"
-                  : "main-insights dynamic"
-              }
-            >
-              <div className="main-header">
-                {articles.data.interests ? (
-                  <button>INSIGHTS FROM YOUR INTERESTS <img src={arrow} alt="Arrow icon" /></button>
-                ) : (
-                  <button>EXPLORE INSIGHTS <img src={arrow} alt="Arrow icon" /></button>
-                )}
-              </div>
-              <div className="main-content">
-                {(articles.data.mainFeed || articles.data.interests) &&
-                  (articles.data.mainFeed || articles.data.interests).map(
-                    article => {
-                      return (
-                        <div
-                          className={
-                            articles.data.following
-                              ? "main-article"
-                              : "main-article dynamic"
-                          }
-                          key={article.id}
-                          onClick={() =>
-                            props.history.push(`/article/${article.custom_id}`)
-                          }
-                        >
-                          <img src={article.coverImageUrl} alt="" />
-                          <div className="main-article-content">
-                            <h3>{article.title}</h3>
-                            <p>
-                              {JSON.parse(article.body)
-                                .find(block => block.type === "paragraph")
-                                .data.text.substring(0, 150)}
-                              ...
-                            </p>
-                            <div className="main-article-footer">
-                              <p>{article.author}</p>
-                              <div className="article-link">
-                                <Link to={`/article/${article.custom_id}`}>Details</Link>
-                                <img src={blue_arrow} alt="Blue Arrow" />
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      );
-                    }
-                  )}
-                {/* <div className="read-more">
-                  <button>Read More...</button>
-                </div> */}
-              </div>
-            </div>
-          )}
-          {!articles.loading &&
-            (articles.data.following || articles.data.reactions) && (
-              <StyledLoggedInFeed>
-                {articles.data.reactions && (
-                  <div className="reactions">
-                    <h4>Reactions</h4>
-                    <div style={{padding: `1.5rem`}}>
-                    {articles.data.reactions.map(reaction => {
-                      return (
-                        <div className="reaction-box" key={reaction.id}>
-                          <p>{reaction.text}</p>
-                        </div>
-                      );
-                    })}
-                    </div>
-                  </div>
-                )}
-
-                {articles.data.following && (
-                  <div className="fav-author-feed">
-                    <h4>Recent Articles from your Favourite Authors</h4>
-                    {articles.data.following.map(article => (
-                      <div
-                        className="fav-author-article"
-                        key={article.id}
-                        onClick={() =>
-                          props.history.push(`/article/${article.custom_id}`)
-                        }
-                      >
-                        <h5>{article.title}</h5>
-                        <h6>{article.author}</h6>
-                        <p>{article.createdAt}</p>
-                        <div className="fav-author-article-footer">
-                          <div className="tags">
-                            {article.tags.map(tag => {
-                              return <p key={tag.id}>{`#${tag.name}`}</p>;
-                            })}
-                          </div>
-                          <div className="details">
-                            <Link to={`/article/${article.custom_id}`}>Details</Link>
-                            <img src={blue_arrow} alt="Arrow" />
-                          </div>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                )}
-              </StyledLoggedInFeed>
+        <StyledFeed>
+          <div className="interests">
+            {!articles.loading && (
+              <h2>
+                {articles.data.interests
+                  ? "INSIGHTS BASED ON YOUR INTERESTS"
+                  : "EXPLORE INSIGHTS"}
+              </h2>
             )}
-        </StyledMainFeed>
-      </StyledFeed>
-    </div>
+            {(articles.data.mainFeed || articles.data.interests) &&
+              (articles.data.interests || articles.data.mainFeed)
+                .slice(0, 3)
+                .map(insight => <ArticleCard insight={insight} />)}
+          </div>
+          <div className="following">
+            {!articles.loading && <h2>INSIGHTS BASED ON YOUR FOLLOWING</h2>}
+          </div>
+        </StyledFeed>
+      </Container>
+    </>
   );
 }
 
