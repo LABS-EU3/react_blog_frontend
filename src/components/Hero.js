81505acb31fa024407241bbbecbb2007e083bb92
Francis Bulus <notchera@gmail.com>
2020-02-05 13:38:51 +0100

    create hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from 'styled-components';
+import theme from '../styles/theme'
+import media from '../styles/mediaQueries'
+
+
+const Hero = () => {
+
+}
+
+export default Hero
