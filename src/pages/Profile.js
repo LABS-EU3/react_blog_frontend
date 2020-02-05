aab8a7f78653fb6f3348241636b9875b767f1184
Megan Ennis <Megan@Megans-iMac-9.local>
2020-02-05 11:48:07 +0000

    Refactor profile to render editable profile info/regular profile info based on location prop

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,41 +1,74 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import Authed from "../components/Navigation/Authed";
-import {  decodeToken } from '../utilities/checkToken';
+import { decodeToken } from "../utilities/checkToken";
 import { verify } from "../redux-store/actions/auths";
-import ProfileCard from './ProfileCard';
+import { Section } from "../styles/shared";
+import styled from "styled-components";
+import {
+  getUserProfile,
+  updateUserProfile
+} from "../redux-store/actions/user-profile-actions";
+import EditProfile from "./EditProfile";
+import AuthorProfile from "./AuthorProfile";
+const Container = styled(Section)`
+  margin-top: 10rem;
+`;
 
+const StyledUserInfo = styled.div``;
+const StyledUserArticles = styled.div``;
 
-function Profile(props) {
+export function Profile(props) {
+  const { subject: userId } = decodeToken();
+  const paramId = JSON.parse(props.location.pathname.split("/")[2]);
+  const { getUserProfile, updateUserProfile, user } = props;
   useEffect(() => {
-    const { subject : userId } = decodeToken();
-    const location = props.location.search; 
-    if (location) {
-      const verificationId = location.split('=');
-      props.verify(verificationId[1], userId)
-    }
-  }, [props, props.location.search])
+    // const location = props.location.search;
+    // if (location) {
+    //   const verificationId = location.split("=");
+    //   props.verify(verificationId[1], userId);
+    // }
+    getUserProfile(paramId);
+    console.log(userId === paramId)
+  }, []);
 
-  if (props.success) {
-    window.location.href = '/profile';
-  }
+  // if (props.success) {
+  //   window.location.href = "/profile";
+  // }
 
   return (
-    <div>
+    <>
       <Authed />
-      <ProfileCard/>
-      {/* <ProfileFirstSection />  
-      <TabsDisplay />           */}
+      <Container>
+          {!user.loading && user.data && 
+        <StyledUserInfo>
+          {userId === paramId ? <>
+          <EditProfile user={user}/> 
+          
+          </> : 
+          //<AuthorProfile user={user}/>
+          null
+          }
+        </StyledUserInfo>
+          }
+        <StyledUserArticles>
 
-    </div>
-  )
+        </StyledUserArticles>
+      </Container>
+    </>
+  );
 }
 
 const mapStateToProps = store => {
   return {
     loading: store.auth.loading,
-    success: store.auth.verification_success
-  }
-}
+    success: store.auth.verification_success,
+    user: store.userProfile
+  };
+};
 
-export default connect(mapStateToProps, {verify})(Profile)
+export default connect(mapStateToProps, {
+  verify,
+  getUserProfile,
+  updateUserProfile
+})(Profile);
