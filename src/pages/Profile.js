b2fd5d141d7f62228149a96ae05152b6b1e770a6
Megan Ennis <Megan@Megans-iMac-9.local>
2020-02-05 14:57:39 +0000

    Plug getArticle function into profile page on mount

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -7,21 +7,28 @@ import { Section } from "../styles/shared";
 import styled from "styled-components";
 import {
   getUserProfile,
-  updateUserProfile
+  getAuthorArticles,
+  followAuthor
 } from "../redux-store/actions/user-profile-actions";
 import EditProfile from "./EditProfile";
-import AuthorProfile from "./AuthorProfile";
+
 const Container = styled(Section)`
   margin-top: 10rem;
 `;
 
-const StyledUserInfo = styled.div``;
+const StyledUserInfo = styled.div`
+  box-shadow: 3px 4px 20px rgba(0, 0, 0, 0.1);
+  min-height: 40vh;
+`;
+
 const StyledUserArticles = styled.div``;
 
 export function Profile(props) {
   const { subject: userId } = decodeToken();
   const paramId = JSON.parse(props.location.pathname.split("/")[2]);
-  const { getUserProfile, updateUserProfile, user } = props;
+  const { getUserProfile, user, followAuthor, articles, getAuthorArticles } = props;
+  const token = localStorage.getItem("token");
+  
   useEffect(() => {
     // const location = props.location.search;
     // if (location) {
@@ -29,31 +36,58 @@ export function Profile(props) {
     //   props.verify(verificationId[1], userId);
     // }
     getUserProfile(paramId);
-    console.log(userId === paramId)
+    getAuthorArticles(paramId);
   }, []);
 
   // if (props.success) {
   //   window.location.href = "/profile";
   // }
 
+  const handleFollow = e => {
+    const button = e.target;
+    followAuthor(paramId).then(res => button.setAttribute("disabled", true));
+  };
+
   return (
     <>
       <Authed />
       <Container>
-          {!user.loading && user.data && 
-        <StyledUserInfo>
-          {userId === paramId ? <>
-          <EditProfile user={user}/> 
-          
-          </> : 
-          //<AuthorProfile user={user}/>
-          null
-          }
-        </StyledUserInfo>
-          }
-        <StyledUserArticles>
+        {!user.loading && user.data && (
+          <StyledUserInfo>
+            {userId === paramId ? <EditProfile user={user}/> :
+            <div className="content">
+              {user.data.followers ? (
+                token &&
+                userId !== paramId &&
+                user.data.followers.includes(userId) ? (
+                  <button>Following</button>
+                ) : (
+                  token &&
+                  userId !== paramId &&
+                  !user.data.followers.includes(userId) && (
+                    <button onClick={handleFollow}>Follow</button>
+                  )
+                )
+              ) : null}
 
-        </StyledUserArticles>
+              <img src={user.data.avatarUrl} alt="" />
+              <h3>{user.data.fullname}</h3>
+              {user.data.bio && <p className="bio">{user.data.bio}</p>}
+              {user.data.followers && user.data.following && (
+                <div className="follows">
+                  <p>
+                    <span>{user.data.followers.length}</span> Followers
+                  </p>
+
+                  <p>
+                    <span>{user.data.following.length}</span> Following
+                  </p>
+                </div>
+              )}
+            </div> }
+          </StyledUserInfo>
+        )}
+        <StyledUserArticles></StyledUserArticles>
       </Container>
     </>
   );
@@ -63,12 +97,15 @@ const mapStateToProps = store => {
   return {
     loading: store.auth.loading,
     success: store.auth.verification_success,
-    user: store.userProfile
+    user: store.userProfile,
+    followSuccess: store.userProfile.followAuthorSuccess,
+    articles: store.userProfile.articles
   };
 };
 
 export default connect(mapStateToProps, {
   verify,
   getUserProfile,
-  updateUserProfile
+  getAuthorArticles,
+  followAuthor
 })(Profile);
