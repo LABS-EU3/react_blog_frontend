a96608f2bb380a989f0d1401aa26a750a2a92bec
Johnson Ogwuru <ogwurujohnson@gmail.com>
2020-02-06 18:06:43 +0100

    fix styling issue with onboarding page

--- src/utilities/styles/final-stage-styles.js
+++ src/utilities/styles/final-stage-styles.js
@@ -4,7 +4,7 @@ import media from "../../styles/mediaQueries";
 export const Wrapper = styled.div`
   display: flex;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   // box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
 `;
 
